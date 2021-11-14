865d3e175428cbe1805760e7de9be0fc921c9ba2
toyoboh <st.0220@softbank.ne.jp>
2021-11-14 22:14:52 +0900

    アプリないでユーザ情報を管理するコンポーネント追加

---
+++ src/context/User.jsx
@@ -0,0 +1,28 @@
+import React, { useState, createContext, useContext } from "react";
+
+const userContext = createContext();
+
+export const useUserContext = () => {
+    return useContext(userContext);
+}
+
+export const UserProvider = ({ children }) => {
+    const initialUserData = {
+        user_id: "",
+        user_name: "",
+        isAuth: false
+    }
+
+    const [user, setUser] = useState(initialUserData);
+
+    const value = {
+        user,
+        setUser
+    }
+
+    return(
+        <userContext.Provider value={ value }>
+            { children }
+        </userContext.Provider>
+    )
+}
