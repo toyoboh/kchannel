1851dd4c7250e74b70356108131f154d23427b01
toyoboh <st.0220@softbank.ne.jp>
2021-11-15 12:21:06 +0900

    App.jsxファイル芙蓉コードの削除

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,11 @@ import { useUserContext } from "./context/User";
 
 function App() {
 
-    const { user, setUser } = useUserContext();
-
-    const update = () => {
-        setUser((prev) => {
-            console.log(prev);
-            //スプレッド構文練習 連想配列ver 重複したkeyの場合は上書きされるよう
-            return { ...prev, ...{ isAuth: !prev.isAuth } };
-            // const test = prev;
-            // console.log("prev", prev);
-            // test.isAuth = !test.isAuth;
-            // console.log("test", test);
-            // return test;
-        })
-    }
-
     return (
         <div className="app">
-            <button onClick={ update }>button</button>
+            
         </div>
     )
 }
 
-export default App
+export default App;
