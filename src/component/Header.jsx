b0fa1c0adbf465810c24e4d53d12099ed07a4b4c
toyoboh <st.0220@softbank.ne.jp>
2021-12-26 21:34:36 +0900

    各画面の高さを修正
    ヘッダーの高さを50pxとし、各画面の最低の高さを100vh - 50pxとした

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../css/Header.css";
-import DesktopWindowsIcon from '@material-ui/icons/DesktopWindows';
 import SearchIcon from "@material-ui/icons/Search";
 import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
 import { useUserContext } from "../context/User";
@@ -13,14 +12,14 @@ const Header = () => {
         <div className="header">
             <div className="header-wrapper">
                 <ul className="header-item-wrapper">
-                    <Link className="header-item" to="">
-                        <li>
-                            <DesktopWindowsIcon />
-                        </li>
-                    </Link>
                     <Link className="header-item" to="/categoryList">
                         <li>
                             <SearchIcon />
+                        </li>
+                    </Link>
+                    <Link className="header-item" to="">
+                        <li>
+                            Kちゃんねる
                         </li>
                     </Link>
                     <Link className="header-item" to={ "/profile/" + user.user_id }>
