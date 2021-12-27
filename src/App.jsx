8dbcd5ab5851d9f404664f7e4e0f98fd4cc636da
toyoboh <st.0220@softbank.ne.jp>
2021-12-27 12:17:32 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./css/App.css";
 // import { useUserContext } from "./context/User";
 import Header from "./component/Header";
