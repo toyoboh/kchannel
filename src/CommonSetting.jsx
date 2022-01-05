7d4298536b3d728a8238f5f3f97255a89d148daf
toyoboh <st.0220@softbank.ne.jp>
2022-01-05 20:05:33 +0900

    axiosのcookie設定

---
+++ src/CommonSetting.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+function CommonSetting() {
+    // cookie
+    axios.defaults.withCredentials = true;
+
+    return <></>
+}
+
+export default CommonSetting;
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/destyle.css";
 import "./css/index.css";
+import CommonSetting from "./CommonSetting";
 import { UserProvider } from "./context/User";
 import LoggedInCheck from "./component/route/LoggedInCheck";
 
 ReactDOM.render(
     <React.StrictMode>
+        <CommonSetting />
         <UserProvider>
             <LoggedInCheck />
         </UserProvider>
