bbb0c083403d5231225f3a612e968895f4d8ab3f
toyoboh <st.0220@softbank.ne.jp>
2022-01-05 18:57:12 +0900

    PHP側でのcookie許可設定

--- backend/Info/settingHeader.php
+++ backend/Info/settingHeader.php
@@ -11,4 +11,5 @@
     header("Content-Type: application/json");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
     header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+    header("Access-Control-Allow-Credentials: true");
 ?>
