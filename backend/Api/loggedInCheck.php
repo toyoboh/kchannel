371db3e2779ecfbc649f4cb0af050d8b1c9b2382
toyoboh <st.0220@softbank.ne.jp>
2022-01-11 12:21:28 +0900

    $_SESSIONによるログイン時の処理変更
    セッションデータによる自動ログインの場合にユーザの最終ログイン日を更新する処理を追加

--- backend/Api/loggedInCheck.php
+++ backend/Api/loggedInCheck.php
@@ -4,22 +4,30 @@ require __DIR__ . "/../Info/settingHeader.php";
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Kchannel\Classes\Tool\Session;
+use Kchannel\Classes\Models\TUser;
 
 if($_SERVER["REQUEST_METHOD"] !== "POST") {
-    echo json_encode([
-        "success" => false
-    ]);
     exit;
 }
 
-$result         = array();
-$result["data"] = array();
+// Define response data
+$result;
 
+// Check if user information remains in the session
 $session = new Session();
 if($session->checkExists("user_id")) {
+    // get session data
+    $user_id = $session->get("user_id");
+    $user_name = $session->get("user_name");
+
+    // Formatting response data
     $result["success"]           = true;
-    $result["data"]["user_id"]   = $session->get("user_id");
-    $result["data"]["user_name"] = $session->get("user_name");
+    $result["data"]["user_id"]   = $user_id;
+    $result["data"]["user_name"] = $user_name;
+
+    // update users last_login_at data 
+    $t_user = new TUser();
+    $t_user->updateAtLogin($user_id);
 } else {
     $result["success"]           = false;
 }
