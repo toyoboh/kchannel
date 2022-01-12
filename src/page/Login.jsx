3572f714fc902fdf3c442f2816dd8adeff5648d0
toyoboh <st.0220@softbank.ne.jp>
2022-01-12 19:20:41 +0900

    update

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import "../css/Login.css";
 import axios from "axios";
 import { useUserContext } from "../context/User";
--- src/page/RegisterAccount.jsx
+++ src/page/RegisterAccount.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 import "../css/RegisterAccount.css";
 import ErrorMessage from "../component/ErrorMessage";
