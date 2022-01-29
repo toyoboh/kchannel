5fd1cc8fa340a0fd0d091fe16a6297555b683c88
toyoboh <st.0220@softbank.ne.jp>
2022-01-30 00:33:36 +0900

    認証クラスの作成

---
+++ src/tool/Auth.jsx
@@ -0,0 +1,32 @@
+class Auth
+{
+    constructor(authority) {
+        this.authority = authority;
+    }
+
+    can(roles) {
+        return roles.indexOf(this.authority) >= 0;
+    }
+
+    createCategory() {
+        this.can([1, 2]);
+    }
+
+    createBoard() {
+        this.can([1, 2]);
+    }
+
+    createThread() {
+        this.can([1, 2]);
+    }
+
+    createComment() {
+        this.can([1, 2]);
+    }
+
+    settingProfile() {
+        this.can([1, 2]);
+    }
+}
+
+export default new Auth();
