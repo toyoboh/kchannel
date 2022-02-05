1151348255efa532b0e1b5140856d3ba10b2048b
toyoboh <st.0220@softbank.ne.jp>
2022-02-05 18:26:14 +0900

    ディレクトリ構造変更
    ディレクトリの構造変更に伴う以下設定ファイルの修正
    ・/kchannel/backend/composer.json
    ・/kchannel/.gitignore

--- frontend/src/info/Url.js
+++ frontend/src/info/Url.js
@@ -5,204 +5,204 @@ const URL = hostName.indexOf(domain) > -1 ?
 {
     csrfToken: { // param: none
         method: "put",
-        url   : "https://api.kchannel.jp/backend/Api/csrfToken.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/csrfToken.php"
     },
     breadcrumbInformation: { // param: pagename, id = default:false
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/breadcrumbInformation.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/breadcrumbInformation.php"
     },
     categoryList: { // param: none
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/categoryList.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/categoryList.php"
     },
     searchCategoryList: { // param: search_word, csrf_token
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/searchCategoryList.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/searchCategoryList.php"
     },
     boardList: { // param: category_id
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/boardList.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/boardList.php"
     },
     searchBoardList: { // param: search_word, category_id, csrf_token
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/searchBoardList.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/searchBoardList.php"
     },
     threadList: { // param: board_id
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/threadList.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/threadList.php"
     },
     fetchThreadInformation: { // param: thread_id
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/fetchThreadInformation.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/fetchThreadInformation.php"
     },
     fetchBoardInformation: { // param: board_id
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/fetchBoardInformation.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/fetchBoardInformation.php"
     },
     fetchCategoryInformation: { // param: category_id
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/fetchCategoryInformation.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/fetchCategoryInformation.php"
     },
     searchThreadList: { // param: search_word, board_id, csrf_token
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/searchThreadList.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/searchThreadList.php"
     },
     commentList: { // param: thread_id
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/commentList.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/commentList.php"
     },
     selectNewComment: { // param: thread_id, comment_id
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/selectNewComment.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/selectNewComment.php"
     },
     createCategory: { // param: category_name, id(t_users), csrf_token
         method: "post",
-        url   : "https://api.kchannel.jp/backend/Api/createCategory.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/createCategory.php"
     },
     createBoard: { // param: category_id, board_name, board_explanation, id, csrf_token
         method: "post",
-        url: "https://api.kchannel.jp/backend/Api/createBoard.php"
+        url: "https://api.kchannel.jp/backend/src/Api/createBoard.php"
     },
     createThread: { // param: board_id, thread_name, thread_explanation, user_id, csrf_token
         method: "post",
-        url   : "https://api.kchannel.jp/backend/Api/createThread.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/createThread.php"
     },
     createComment: { // param: thread_id, comment_body, user_id, csrf_token
         method: "post",
-        url   : "https://api.kchannel.jp/backend/Api/createComment.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/createComment.php"
     },
     checkCategoryExists: { // param: category_id
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/checkCategoryExists.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/checkCategoryExists.php"
     },
     userProfile: { // param: user_id
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/userProfile.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/userProfile.php"
     },
     updateUserProfile: { // param: id, user_name, introduction, csrf_token
         method: "post",
-        url   : "https://api.kchannel.jp/backend/Api/updateUserProfile.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/updateUserProfile.php"
     },
     login: {
         method: "post",
-        url   : "https://api.kchannel.jp/backend/Api/login.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/login.php"
     },
     loggedInCheck: {
         method: "post",
-        url   : "https://api.kchannel.jp/backend/Api/loggedInCheck.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/loggedInCheck.php"
     },
     logout: { // param: none
         method: "get",
-        url   : "https://api.kchannel.jp/backend/Api/logout.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/logout.php"
     },
     temporaryRegistration: {
         method: "post",
-        url   : "https://api.kchannel.jp/backend/Api/temporaryRegistration.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/temporaryRegistration.php"
     },
     accountRegistration: {
         method: "post",
-        url   : "https://api.kchannel.jp/backend/Api/accountRegistration.php"
+        url   : "https://api.kchannel.jp/backend/src/Api/accountRegistration.php"
     }
 } : {
     csrfToken: { // param: none
         method: "put",
-        url   : "http://localhost:49002/backend/Api/csrfToken.php"
+        url   : "http://localhost:49002/backend/src/Api/csrfToken.php"
     },
     breadcrumbInformation: { // param: pagename, id = default:false
         method: "get",
-        url   : "http://localhost:49002/backend/Api/breadcrumbInformation.php"
+        url   : "http://localhost:49002/backend/src/Api/breadcrumbInformation.php"
     },
     categoryList: { // param: none
         method: "get",
-        url   : "http://localhost:49002/backend/Api/categoryList.php"
+        url   : "http://localhost:49002/backend/src/Api/categoryList.php"
     },
     searchCategoryList: { // param: search_word, csrf_token
         method: "get",
-        url   : "http://localhost:49002/backend/Api/searchCategoryList.php"
+        url   : "http://localhost:49002/backend/src/Api/searchCategoryList.php"
     },
     boardList: { // param: category_id
         method: "get",
-        url   : "http://localhost:49002/backend/Api/boardList.php"
+        url   : "http://localhost:49002/backend/src/Api/boardList.php"
     },
     searchBoardList: { // param: search_word, category_id, csrf_token
         method: "get",
-        url   : "http://localhost:49002/backend/Api/searchBoardList.php"
+        url   : "http://localhost:49002/backend/src/Api/searchBoardList.php"
     },
     threadList: { // param: board_id
         method: "get",
-        url   : "http://localhost:49002/backend/Api/threadList.php"
+        url   : "http://localhost:49002/backend/src/Api/threadList.php"
     },
     fetchThreadInformation: { // param: thread_id
         method: "get",
-        url   : "http://localhost:49002/backend/Api/fetchThreadInformation.php"
+        url   : "http://localhost:49002/backend/src/Api/fetchThreadInformation.php"
     },
     fetchBoardInformation: { // param: board_id
         method: "get",
-        url   : "http://localhost:49002/backend/Api/fetchBoardInformation.php"
+        url   : "http://localhost:49002/backend/src/Api/fetchBoardInformation.php"
     },
     fetchCategoryInformation: { // param: category_id
         method: "get",
-        url   : "http://localhost:49002/backend/Api/fetchCategoryInformation.php"
+        url   : "http://localhost:49002/backend/src/Api/fetchCategoryInformation.php"
     },
     searchThreadList: { // param: search_word, board_id, csrf_token
         method: "get",
-        url   : "http://localhost:49002/backend/Api/searchThreadList.php"
+        url   : "http://localhost:49002/backend/src/Api/searchThreadList.php"
     },
     commentList: { // param: thread_id
         method: "get",
-        url   : "http://localhost:49002/backend/Api/commentList.php"
+        url   : "http://localhost:49002/backend/src/Api/commentList.php"
     },
     selectNewComment: { // param: thread_id, comment_id
         method: "get",
-        url   : "http://localhost:49002/backend/Api/selectNewComment.php"
+        url   : "http://localhost:49002/backend/src/Api/selectNewComment.php"
     },
     createCategory: { // param: category_name, id(t_users), csrf_token
         method: "post",
-        url   : "http://localhost:49002/backend/Api/createCategory.php"
+        url   : "http://localhost:49002/backend/src/Api/createCategory.php"
     },
     createBoard: { // param: category_id, board_name, board_explanation, id, csrf_token
         method: "post",
-        url: "http://localhost:49002/backend/Api/createBoard.php"
+        url: "http://localhost:49002/backend/src/Api/createBoard.php"
     },
     createThread: { // param: board_id, thread_name, thread_explanation, user_id, csrf_token
         method: "post",
-        url   : "http://localhost:49002/backend/Api/createThread.php"
+        url   : "http://localhost:49002/backend/src/Api/createThread.php"
     },
     createComment: { // param: thread_id, comment_body, user_id, csrf_token
         method: "post",
-        url   : "http://localhost:49002/backend/Api/createComment.php"
+        url   : "http://localhost:49002/backend/src/Api/createComment.php"
     },
     checkCategoryExists: { // param: category_id
         method: "get",
-        url   : "http://localhost:49002/backend/Api/checkCategoryExists.php"
+        url   : "http://localhost:49002/backend/src/Api/checkCategoryExists.php"
     },
     userProfile: { // param: user_id
         method: "get",
-        url   : "http://localhost:49002/backend/Api/userProfile.php"
+        url   : "http://localhost:49002/backend/src/Api/userProfile.php"
     },
     updateUserProfile: { // param: id, user_name, introduction, csrf_token
         method: "post",
-        url   : "http://localhost:49002/backend/Api/updateUserProfile.php"
+        url   : "http://localhost:49002/backend/src/Api/updateUserProfile.php"
     },
     login: {
         method: "post",
-        url   : "http://localhost:49002/backend/Api/login.php"
+        url   : "http://localhost:49002/backend/src/Api/login.php"
     },
     loggedInCheck: {
         method: "post",
-        url   : "http://localhost:49002/backend/Api/loggedInCheck.php"
+        url   : "http://localhost:49002/backend/src/Api/loggedInCheck.php"
     },
     logout: { // param: none
         method: "get",
-        url   : "http://localhost:49002/backend/Api/logout.php"
+        url   : "http://localhost:49002/backend/src/Api/logout.php"
     },
     temporaryRegistration: {
         method: "post",
-        url   : "http://localhost:49002/backend/Api/temporaryRegistration.php"
+        url   : "http://localhost:49002/backend/src/Api/temporaryRegistration.php"
     },
     accountRegistration: {
         method: "post",
-        url   : "http://localhost:49002/backend/Api/accountRegistration.php"
+        url   : "http://localhost:49002/backend/src/Api/accountRegistration.php"
     }
 }
 
